bef1a26af73a4695a9663ffd1be487e5b6f0095d
limelier <tudor.iacobescu.1@gmail.com>
2020-03-09 10:58:39 +0200

    Initial commit 2

---
+++ src/com/company/Cart.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Vector;
+
+public class Cart {
+
+    /**
+   * 
+   * @element-type Item
+   */
+  public Vector  contents;
+
+  public Integer total() {
+  return null;
+  }
+
+  public void checkout() {
+  }
+
+}
---
+++ src/com/company/Item.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Item {
+
+  public Integer qty;
+
+  public Integer price;
+
+  public String name;
+
+    
+}
